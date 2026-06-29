3277666ab3e7bbbba2485c59e5efd1dfc6a90af1
sarojasgarrido <s.a.rojasgarrido@gmail.com>
2026-06-29 17:54:32 -0400

    db.py

--- db.py
+++ db.py
@@ -1,19 +1,10 @@
-import os
 import psycopg2
+import os
 from dotenv import load_dotenv
 
-# Carga las variables de entorno (asegúrate de que DATABASE_URL esté en Render)
 load_dotenv()
 
 def get_connection():
-    """
-    Crea y devuelve una conexión a la base de datos PostgreSQL (Neon).
-    """
-    # La variable DATABASE_URL ya contiene toda la información de conexión
-    # que te da Neon/Render.
-    url_db = os.environ.get("DATABASE_URL")
-    
-    if not url_db:
-        raise ValueError("DATABASE_URL no está configurada en las variables de entorno.")
-        
-    return psycopg2.connect(url_db)
+    # Neon proporciona una URL completa. Psycopg2 la usa directamente.
+    db_url = os.getenv("DATABASE_URL")
+    return psycopg2.connect(db_url)
