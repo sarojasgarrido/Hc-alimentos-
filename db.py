5f4b9a342657d608b67a529b7fd057e8d0683c7a
sarojasgarrido <s.a.rojasgarrido@gmail.com>
2026-06-29 22:41:51 -0400

    db.py

--- db.py
+++ db.py
@@ -6,28 +6,29 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def get_connection():
-    """
-    Crea una conexión a Neon. 
-    Lanza un error claro si la variable de entorno no existe.
-    """
     db_url = os.getenv("DATABASE_URL")
     if not db_url:
-        raise Exception("Error: La variable DATABASE_URL no está definida en tu archivo .env")
-    
+        raise Exception("DATABASE_URL no configurada")
     return psycopg2.connect(db_url)
 
-# Helper para ejecutar consultas y cerrar automáticamente
+# Para consultas SELECT
 def ejecutar_query(query, params=None):
-    """
-    Función utilitaria para consultas que no requieren transacción (SELECTs).
-    Abre y cierra la conexión automáticamente.
-    """
     conn = get_connection()
     cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
     try:
         cur.execute(query, params)
-        resultado = cur.fetchall()
-        return resultado
+        return cur.fetchall()
+    finally:
+        cur.close()
+        conn.close()
+
+# Para INSERT, UPDATE, DELETE (Operaciones que cambian datos)
+def ejecutar_comando(query, params=None):
+    conn = get_connection()
+    cur = conn.cursor()
+    try:
+        cur.execute(query, params)
+        conn.commit() # ¡Esto es lo que faltaba!
     finally:
         cur.close()
         conn.close()
